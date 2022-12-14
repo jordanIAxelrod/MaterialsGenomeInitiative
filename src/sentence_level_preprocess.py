c62e99fab3a90982d9e24bc09e7c57db3b339be9
shtmk <shota.miki@duke.edu>
2022-12-13 22:39:23 -0500

    Modify preprocessing

--- src/sentence_level_preprocess.py
+++ src/sentence_level_preprocess.py
@@ -33,6 +33,29 @@ def rm_linebreaks(text: str, p=False) -> str:
 
 
 
+def put_ratio_together(w: str) -> str:
+    """
+    Transform a ratio expressed as a string into '<ratio>'.
+    e.g.
+    '1:1' -> '<ratio>'
+
+    args:
+      - w: a string (word)
+
+    returns:
+      - w or '<ratio>'
+    """
+
+    """
+    if re.fullmatch(r"[0-9]+:[0-9]+", w) is None:
+        return w
+    else:
+        return '<ratio>'
+    """
+    return re.sub(r"<(int|dec)> : <(int|dec)>", "<ratio>", w)
+
+
+
 def c2temp_2(text: str) -> str:
     """
     Transform two-words expressions of degrees celsius into '<temp>'.
@@ -49,3 +72,22 @@ def c2temp_2(text: str) -> str:
     text_t = re.sub(r"° c", '<temp>', text)
     text_t = re.sub(r"◦ c", '<temp>', text)
     return text_t
+
+
+
+def split_with_keys(s: str, keys: str) -> str:
+    """
+    Split with a key.
+    e.g.
+    When key='(', '(1:1)' -> '( 1:1 )'
+    When key='.', 'done.' -> 'done .'
+
+    args:
+      - s: a string (sentence)
+
+    returns:
+      - a string
+    """
+    for k in keys:
+        s = s.replace(k, ' '+k+' ').replace('  ', ' ').strip()
+    return s
