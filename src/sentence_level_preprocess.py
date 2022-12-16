04523c372626fb84e40b0915c37a0808b5aca6f8
Logan Cooper <ldtcoop@gmail.com>
2022-12-15 20:32:36 -0500

    preprocessing Move preprocessing function

--- src/sentence_level_preprocess.py
+++ src/sentence_level_preprocess.py
@@ -95,3 +95,34 @@ def split_with_keys(s: str, keys: str) -> str:
     for k in keys:
         s = s.replace(k, ' '+k+' ').replace('  ', ' ').strip()
     return s
+
+
+def preprocess(data: numpy_records) -> numpy_records:
+    # remove line breaks (e.g. "elec- tron" -> "electron")
+    data = [(rm_linebreaks(t), l) for t, l in data]
+
+    # lower case
+    data = [(t.lower(), l) for t, l in data]
+
+    # unify expressions for temperature (e.g. '° c' -> '<temp>')
+    data = [(c2temp_2(t), l) for t, l in data]
+
+    data = [(nltk.tokenize.word_tokenize(t), l) for t, l in data]
+
+    # recognize integer as '<int>' (e.g. '60' -> '<int>')
+    data = [([put_int_together(w) for w in t], l) for t, l in data]
+
+    # recognize decimal as '<dec>' (e.g. '0.5' -> '<dec>')
+    data = [([put_decimal_together(w) for w in t], l) for t, l in data]
+
+    # recognize ratioas '<ratio>' (e.g. '1:1' -> '<ratio>')
+    data = [([put_ratio_together(w) for w in t], l) for t, l in data]
+
+    # split slash (e.g. 'g/mol' -> '['g', '/', 'mol'])
+    data = [([split_slash(w) for w in t], l) for t, l in data]
+    data = [(list(itertools.chain.from_iterable(t)), l) for t, l in data] # flatten
+
+    # unify expressions for temperature (e.g. '°c' -> '<temp>')
+    data = [([c2temp(w) for w in t], l) for t, l in data]
+    
+    return data
